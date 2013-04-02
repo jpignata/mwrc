5fc54dcab650e59411ccac9b72150b8475f23135
John Pignata <john@pignata.com>
2013-04-02 17:28:05 -0400

    Extract smaller methods out of PushDaemon#start
    Move chunks of start into private methods. We could continue extracting
    out many more smaller pieces, but most of this will be moved out of
    PushDaemon so we'll leave it mostly in tact for now.

--- lib/push_daemon.rb
+++ lib/push_daemon.rb
@@ -14,6 +14,14 @@ class PushDaemon
   end
 
   def start
+    spawn_workers
+    bind
+    loop { process_request }
+  end
+
+  private
+
+  def spawn_workers
     10.times do
       Thread.new do
         while data = @queue.pop
@@ -24,21 +32,24 @@ class PushDaemon
         end
       end
     end
+  end
 
+  def bind
     @socket.bind("0.0.0.0", 6889)
+  end
 
-    while data = @socket.recvfrom(4096)
-      case data[0].split.first
-      when "PING"
-        @socket.send("PONG", 0, data[1][3], data[1][1])
-      when "SEND"
-        data[0][5..-1].match(/([a-zA-Z0-9_\-]*) "([^"]*)/)
-        json = JSON.generate({
-          "registration_ids" => [$1],
-          "data" => { "alert" => $2 }
-        })
-        @queue << json
-      end
+  def process_request
+    data = @socket.recvfrom(4096)
+    case data[0].split.first
+    when "PING"
+      @socket.send("PONG", 0, data[1][3], data[1][1])
+    when "SEND"
+      data[0][5..-1].match(/([a-zA-Z0-9_\-]*) "([^"]*)/)
+      json = JSON.generate({
+        "registration_ids" => [$1],
+        "data" => { "alert" => $2 }
+      })
+      @queue << json
     end
   end
 end
